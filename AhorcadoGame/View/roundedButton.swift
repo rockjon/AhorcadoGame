85f17aa0c93ddeb7e65956f7744c66ad22929f8a
jonathan hernandez <jonathanhernandez@jonathans-imac>
2017-12-12 16:06:14 -0600

    Icon app, and bg

--- AhorcadoGame/View/roundedButton.swift
+++ AhorcadoGame/View/roundedButton.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
-
+@IBDesignable
 class roundedButton: UIButton {
 
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    func customizeView() {
         layer.cornerRadius = self.frame.width/2
         layer.backgroundColor = UIColor(red: CGFloat(149/255.0), green: CGFloat(165/255.0), blue: CGFloat(166/255.0), alpha: CGFloat(1.0)).cgColor
     }
-    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
